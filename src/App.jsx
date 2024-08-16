e5e3091c6efabf02141a1cbe66363e3a650e2325
SamRCrespo <samcre8997@students.ecpi.edu>
2024-08-16 16:59:03 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a simple React application for me.</p>
+    </div>
+  );
+}
+
+export default App;
